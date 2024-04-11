40e940b0f359e1ed2ad36c54e60fbadf4738209e
GitUser6910 <idinahuiti890@gmail.com>
2024-04-11 19:20:18 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
-        // Random rand = new Random();
+        Random rand = new Random();
+        boolean isStopped = false;
         String[] fruitsArr = {
                 "사과 - яблоко",
                 "배 - груша",
@@ -34,7 +35,7 @@ public class Main {
         String[] fruitsQuiz = {
                 "яблоко",
                 "груша",
-                "банан ",
+                "банан",
                 "виноград",
                 "апельсин",
                 "лимон",
@@ -57,45 +58,212 @@ public class Main {
                 "инжир",
                 "мангостин"
         };
-        System.out.println("Корейский язык");
-        System.out.println("--------------\n");
-        System.out.println("1 - Викторина");
-        System.out.println("2 - Тренеровка письма");
-        System.out.print("--Введите вариант: ");
-        int option = scanner.nextInt();
-        scanner.nextLine();
-        switch (option) {
-            case 2:
-                System.out.println("1 - Фрукты");
-                System.out.println("2 - Овощи");
-                System.out.print("Введите тему: ");
-                int quiz = scanner.nextInt();
-                scanner.nextLine();
-                switch (quiz) {
-                    case 1:
-                        System.out.println("Нажимайте 'Enter' после каждого слова если вы его запомнили");
-                        for (int i = 0; i < fruitsArr.length; i++) {
-                            System.out.print(fruitsArr[i]);
-                            scanner.nextLine();
-                        }
-                        System.out.print("\033[H\033[2J");
-                        System.out.flush();
-                        System.out.println("А теперь викторина. Удачи!");
-                        for (int i = 0; i < fruitsQuiz.length; i++) {
-                            System.out.print(fruitsQuiz[i] + " - ");
-                            String quizVal = scanner.nextLine();
-                            String koreanWord = fruitsArr[i].split(" - ")[0];
-                            if (quizVal.equalsIgnoreCase(koreanWord)) {
-                                System.out.println("Правильно");
-                            } else {
-                                System.out.println("Ошибка");
+        String[] vegetablesArr = {
+                "가지 - баклажан",
+                "콩 - фасоль",
+                "완두콩 - горох",
+                "양배추 - капуста",
+                "감자 - картофель",
+                "양파 - лук",
+                "당근 - морковь",
+                "오이 - огурец",
+                "후추 - перец",
+                "파슬리 - петрушка",
+                "토마토 - помидор",
+                "무 - редис",
+                "사탕무우 - свекла",
+                "셀러리 - сельдерей",
+                "호박 - тыква",
+                "딜 - укроп",
+                "콩 - фасоль",
+                "마늘 - чеснок",
+                "밤색 - щавель"
+        };
+        String[] vegetablesQuiz = {
+                "баклажан",
+                "фасоль",
+                "горох",
+                "капуста",
+                "картофель",
+                "лук",
+                "морковь",
+                "огурец",
+                "перец",
+                "петрушка",
+                "помидор",
+                "редис",
+                "свекла",
+                "сельдерей",
+                "тыква",
+                "укроп",
+                "фасоль",
+                "чеснок",
+                "щавель"
+        };
+        try {
+            System.out.println("Корейский язык");
+            System.out.println("--------------");
+            System.out.println("1 - Викторина");
+            System.out.println("2 - Тренеровка письма");
+            System.out.print("--Введите вариант: ");
+            int option = scanner.nextInt();
+            scanner.nextLine();
+            switch (option) {
+                case 1:
+                    System.out.println("1 - Фрукты");
+                    System.out.println("2 - Овощи");
+                    System.out.print("Введите тему: ");
+                    int quiz = scanner.nextInt();
+                    scanner.nextLine();
+                    switch (quiz) {
+                        case 1:
+                            System.out.println("Нажимайте 'Enter' после каждого слова если вы его запомнили");
+                            for (int i = 0; i < fruitsArr.length; i++) {
+                                System.out.print(fruitsArr[i]);
+                                scanner.nextLine();
                             }
-                        }
-                        break;
-                }
-                break;
+                            System.out.print("\033[H\033[2J");
+                            System.out.flush();
+                            System.out.println("А теперь викторина. Удачи!");
+                            List<String> wordsFruits = new ArrayList<>(List.of(fruitsQuiz));
+                            for (int j = 0; j < fruitsQuiz.length; j++) {
+                                List<String> fruitsOptions = new ArrayList<>();
+                                fruitsOptions.add(fruitsArr[j].split(" - ")[0]); // Правильный ответ
+
+                                // Добавление двух случайных неправильных ответов
+                                while (fruitsOptions.size() < 3) {
+                                    int randomIndex = rand.nextInt(fruitsArr.length);
+                                    String randomWord = fruitsArr[randomIndex].split(" - ")[0];
+                                    if (!fruitsOptions.contains(randomWord) && !wordsFruits.get(j).equals(randomWord)) {
+                                        fruitsOptions.add(randomWord);
+                                    }
+                                }
+
+                                // Перемешивание вариантов ответов
+                                Collections.shuffle(fruitsOptions, rand);
+
+                                System.out.println("--" + fruitsQuiz[j]);
+                                for (int i = 0; i < fruitsOptions.size(); i++) {
+                                    System.out.println((i + 1) + " - " + fruitsOptions.get(i));
+                                }
+                                System.out.print("Ваш ответ: ");
+                                int answer = scanner.nextInt();
+                                scanner.nextLine();
+                                if (fruitsOptions.get(answer - 1).equals(fruitsArr[j].split(" - ")[0])) {
+                                    System.out.println("Правильно\n");
+                                } else {
+                                    System.err.println("Ошибка\n");
+                                }
+                            }
+                            break;
+                        case 2:
+                            System.out.println("Нажимайте 'Enter' после каждого слова если вы его запомнили");
+                            for (int i = 0; i < vegetablesArr.length; i++) {
+                                System.out.print(vegetablesArr[i]);
+                                scanner.nextLine();
+                            }
+                            System.out.print("\033[H\033[2J");
+                            System.out.flush();
+                            System.out.println("А теперь викторина. Удачи!");
+                            List<String> wordsVegetables = new ArrayList<>(List.of(vegetablesQuiz));
+                            for (int j = 0; j < vegetablesQuiz.length; j++) {
+                                List<String> VegetablesOptions = new ArrayList<>();
+                                VegetablesOptions.add(vegetablesArr[j].split(" - ")[0]); // Правильный ответ
+
+                                // Добавление двух случайных неправильных ответов
+                                while (VegetablesOptions.size() < 3) {
+                                    int randomIndex = rand.nextInt(vegetablesArr.length);
+                                    String randomWord = vegetablesArr[randomIndex].split(" - ")[0];
+                                    if (!VegetablesOptions.contains(randomWord) && !wordsVegetables.get(j).equals(randomWord)) {
+                                        VegetablesOptions.add(randomWord);
+                                    }
+                                }
+
+                                // Перемешивание вариантов ответов
+                                Collections.shuffle(VegetablesOptions, rand);
+
+                                System.out.println("--" + vegetablesQuiz[j]);
+                                for (int i = 0; i < VegetablesOptions.size(); i++) {
+                                    System.out.println((i + 1) + " - " + VegetablesOptions.get(i));
+                                }
+                                System.out.print("Ваш ответ: ");
+                                int answer = scanner.nextInt();
+                                scanner.nextLine();
+                                if (VegetablesOptions.get(answer - 1).equals(vegetablesArr[j].split(" - ")[0])) {
+                                    System.out.println("Правильно\n");
+                                } else {
+                                    System.err.println("Ошибка\n");
+                                }
+                            }
+                            break;
+                    }
+                    break;
+                case 2:
+                    System.out.println("1 - Фрукты");
+                    System.out.println("2 - Овощи");
+                    System.out.print("Введите тему: ");
+                    int quizWrite = scanner.nextInt();
+                    scanner.nextLine();
+                    switch (quizWrite) {
+                        case 1:
+                            System.out.println("Нажимайте 'Enter' после каждого слова если вы его запомнили");
+                            for (int i = 0; i < fruitsArr.length; i++) {
+                                System.out.print(fruitsArr[i]);
+                                scanner.nextLine();
+                            }
+                            System.out.print("\033[H\033[2J");
+                            System.out.flush();
+                            System.out.println("А теперь письменное закрепление материала. Удачи!");
+                            // scanner.close();
+                            for (int i = 0; i < fruitsArr.length; i++) {
+                                String[] pair = fruitsArr[i].split(" - ");
+                                String koreanWord = pair[0];
+                                String russianWord = pair[1];
+                                System.out.print(russianWord + " - ");
+                                String userInput = scanner.nextLine();
+
+                                if (userInput.equalsIgnoreCase(koreanWord)) {
+                                    System.out.println("Правильно");
+                                } else {
+                                    System.out.println("Ошибка. Правильный ответ: " + koreanWord);
+                                }
+                            }
+                            break;
+                        case 2:
+                            System.out.println("Нажимайте 'Enter' после каждого слова если вы его запомнили");
+                            for (int i = 0; i < vegetablesArr.length; i++) {
+                                System.out.print(vegetablesArr[i]);
+                                scanner.nextLine();
+                            }
+                            System.out.print("\033[H\033[2J");
+                            System.out.flush();
+                            System.out.println("А теперь письменное закрепление материала. Удачи!");
+                            // scanner.close();
+                            for (int i = 0; i < vegetablesArr.length; i++) {
+                                String[] pair = vegetablesArr[i].split(" - ");
+                                String koreanWord = pair[0];
+                                String russianWord = pair[1];
+                                System.out.print(russianWord + " - ");
+                                String userInput = scanner.nextLine();
+
+                                if (userInput.equalsIgnoreCase(koreanWord)) {
+                                    System.out.println("Правильно");
+                                } else {
+                                    System.out.println("Ошибка. Правильный ответ: " + koreanWord);
+                                }
+                            }
+                            break;
+                    }
+                    break;
+                default:
+                    System.err.println("Вы ввели неверный вариант");
+                    break;
+            }
+        } catch (InputMismatchException e) {
+            System.err.println("Введите достоверные данные!");
         }
-        scanner.nextLine();
+
+        // scanner.nextLine();
     }
 
     private static void clearScreen() throws IOException, InterruptedException {
